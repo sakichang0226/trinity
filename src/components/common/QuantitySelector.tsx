f065cab4a7f341bfefda81d3dace247babe64841
sakichange0226 <saki.develop0226@gmail.com>
2024-11-11 23:52:16 +0900

    fix:コンポーネントの丸みをなくす

--- src/components/common/QuantitySelector.tsx
+++ src/components/common/QuantitySelector.tsx
@@ -10,36 +10,36 @@ const QuantitySelector = (props: {
 }) => {
     return (
         <div className="inline-flex flex-col items-center">
-        <div className="flex border border-neutral-300 rounded-md">
-          <SfButton
-            variant="tertiary"
-            square
-            className="rounded-r-none"
-            disabled={props.value <= props.min}
-            aria-label="Decrease value"
-            onClick={() => props.setValue(props.value - 1) }
-          >
-            <SfIconRemove />
-          </SfButton>
-          <input
-            type="number"
-            role="spinbutton"
-            className="appearance-none mx-2 w-8 text-center bg-transparent font-medium [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:display-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:display-none [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none disabled:placeholder-disabled-900 focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm"
-            min={props.min}
-            max={props.max}
-            value={props.value}
-            onChange={(e) => props.setValue(e.target.value)}
-          />
-          <SfButton
-            variant="tertiary"
-            square
-            className="rounded-l-none"
-            disabled={props.value >= props.max}
-            aria-label="Increase value"
-            onClick={() => props.setValue(props.value + 1) }
-          >
-            <SfIconAdd />
-          </SfButton>
+          <div className="flex border border-neutral-300">
+            <SfButton
+              variant="tertiary"
+              square
+              className="rounded-r-none"
+              disabled={props.value <= props.min}
+              aria-label="Decrease value"
+              onClick={() => props.setValue(props.value - 1) }
+            >
+              <SfIconRemove />
+            </SfButton>
+            <input
+              type="number"
+              role="spinbutton"
+              className="appearance-none mx-2 w-8 text-center bg-transparent font-medium [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-inner-spin-button]:display-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-outer-spin-button]:display-none [&::-webkit-outer-spin-button]:m-0 [-moz-appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none disabled:placeholder-disabled-900 focus-visible:outline focus-visible:outline-offset focus-visible:rounded-sm"
+              min={props.min}
+              max={props.max}
+              value={props.value}
+              onChange={(e) => props.setValue(e.target.value)}
+            />
+            <SfButton
+              variant="tertiary"
+              square
+              className="rounded-l-none"
+              disabled={props.value >= props.max}
+              aria-label="Increase value"
+              onClick={() => props.setValue(props.value + 1) }
+            >
+              <SfIconAdd />
+            </SfButton>
         </div>
       </div>
     ) 
--- src/components/items/AddToCartButton.tsx
+++ src/components/items/AddToCartButton.tsx
@@ -3,5 +3,5 @@ import { SfButton } from "@storefront-ui/react"
 export const AddToCartButton = (props : {
     isEnabled? : boolean
 }) => {
-    return <><SfButton disabled={props.isEnabled}>{ props.isEnabled ? "SOLD OUT" : "ADD TO CART" }</SfButton></>
+    return <><SfButton className="rounded-none" disabled={props.isEnabled}>{ props.isEnabled ? "SOLD OUT" : "ADD TO CART" }</SfButton></>
 }
